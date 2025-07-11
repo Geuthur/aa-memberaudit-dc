aabab30d7309496dfdade0cca2433888745d026f
Geuthur <geuthur@users.noreply.github.com>
2025-07-11 09:57:34 +0200

    [CHANGE] use `django-esi` new guidelines

--- madc/__init__.py
+++ madc/__init__.py
@@ -2,3 +2,8 @@
 
 __version__ = "0.2.2"
 __title__ = "AA Memberaudit Doctrine Checker"
+
+__package_name__ = "aa-memberaudit-dc"
+
+__app_name_useragent__ = "AA-Memberaudit-DC"
+__github_url__ = f"https://github.com/Geuthur/{__package_name__}"
--- madc/providers.py
+++ madc/providers.py
@@ -4,8 +4,10 @@
 from esi.clients import EsiClientProvider
 
 # AA Memberaudit Doctrine Checker
-from madc.constants import USER_AGENT
+from madc import __app_name_useragent__, __github_url__, __version__
 from madc.helpers.skill_handler import SkillListHandler
 
-esi = EsiClientProvider(app_info_text=USER_AGENT)
+esi = EsiClientProvider(
+    ua_appname=__app_name_useragent__, ua_version=__version__, ua_url=__github_url__
+)
 skills = SkillListHandler()
