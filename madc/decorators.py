b846617233958794fcdb452fd7ea2922364f4b60
Geuthur <geuthur@users.noreply.github.com>
2025-10-28 12:41:55 +0100

    [REMOVE] Unused Stuff

--- madc/decorators.py
+++ madc/decorators.py
@@ -1,13 +1,11 @@
 # Standard Library
 import sys
 import time
-from functools import wraps
 
 # Alliance Auth
 from allianceauth.services.hooks import get_extension_logger
 
 # Alliance Auth (External Libs)
-from app_utils.esi import EsiDailyDowntime, fetch_esi_status
 from app_utils.logging import LoggerAddTag
 
 # AA Memberaudit Doctrine Checker
@@ -17,29 +15,6 @@ logger = LoggerAddTag(get_extension_logger(__name__), __title__)
 
 # True when tests are currently running, else False.
 IS_TESTING = sys.argv[1:2] == ["test"]
-
-
-def when_esi_is_available(func):
-    """Make sure the decorated task only runs when esi is available.
-
-    Raise exception when ESI is offline.
-    Complete the task without running it when downtime is detected.
-
-    Automatically disabled during tests.
-    """
-
-    @wraps(func)
-    def outer(*args, **kwargs):
-        if IS_TESTING is not True:
-            try:
-                fetch_esi_status().raise_for_status()
-            except EsiDailyDowntime:
-                logger.info("Daily Downtime detected. Aborting.")
-                return None  # function will not run
-
-        return func(*args, **kwargs)
-
-    return outer
 
 
 def log_timing(logs):
